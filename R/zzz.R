ee28b7ec8c7c063a92c9ab7859dd44d77cc2aac5
Dan Tenenbaum <dtenenba@fhcrc.org>
2011-10-20 20:12:58 +0000

    For all packages without a namespace and with a .First.lib() function, did the following:
     - Replaced .First.lib() with .onLoad() if function did something
       useful (otherwise removed function).
     - If there is an .onLoad() function, added a generic NAMESPACE file.
     - Bumped version.
    
    Not adding NAMESPACE files to packages without .First.lib()/.onLoad(),
    as the build system will generate those when package versions are bumped
    (when we release BioC 2.9).
    
    
    
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/kidpack@1778 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 ## dummy
-.First.lib <- function(libname, pkgname, where)
+.onLoad <- function(libname, pkgname)
  message(sprintf("\n*** Deprecation warning ***:\nThe package '%s' is deprecated and will not be supported after Bioconductor release 2.1.\n\n", pkgname))
